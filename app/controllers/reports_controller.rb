de8dabf9173f0363c79d1506eafbd000cbc07f2f
Dmitry Mozzherin <dmozzherin@eol.org>
2008-08-05 12:27:28 +0000

    work on reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,7 +1,14 @@
 class ReportsController < ApplicationController
   layout 'user'
   def index
+    page = params[:page] || 1 rescue 1
     @project = Project.find(params[:project_id])
+    @reports = Report.find_by_sql(["select r.* from reports r join iterations i on i.id = r.iteration_id where i.project_id = ? and i.end_date < ? order by i.start_date desc", @project.id, Date.today])
+  end
+
+  def show
+    @project = Project.find(params[:project_id])
+    @report = Report.find(params[:id])
   end
 
 protected
